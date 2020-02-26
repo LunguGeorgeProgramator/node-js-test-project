04c4713f688c2514e192bbbe6aedd444374bda2d
Lungu George <lungugeorge111@gmail.com>
2020-02-26 15:34:21 +0200

    set init files

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http');
-
+var express = require('express');
+const app = express()
 const hostname = '0.0.0.0';
 const port = 6060;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
+
+
+app.listen(port, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
